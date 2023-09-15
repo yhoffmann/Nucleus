fc75668df3d7b52c4c8dc859e3612a7e3b24eb4e
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2023-09-15 10:33:25 +0200

    Adding constants.hpp.

---
+++ include/constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+
+const double nbTofm2 = 1.0e-7;
+const double fm2Tonb = 1.0/nbTofm2;
+
+const double HBAR_C = 0.1973;
+
+const double GeVTofmm1 = 1.0/HBAR_C;
+const double fmToGeVm1 = 1.0/HBAR_C;
+const double GeVm1Tofm = HBAR_C;
+const double fmm1ToGeV = HBAR_C;
