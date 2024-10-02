e8cff857c7da25d7f4a688ff1a804461ed9b3b5a
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2024-10-02 16:48:58 +0200

    Moved NuclearParameters::get::params into NuclearParameters.

--- include/NuclearParameters.hpp
+++ include/NuclearParameters.hpp
@@ -30,24 +30,24 @@ struct NucleusParameters
 
 namespace NuclearParameters
 {
-    NucleusParameters get (uint atomic_num)
-    {
-        // from source [1] and [2]
-        static const NucleusParameters params[] =
-        { // R,     a,      w               Symbol  A           index
-            {1.0,   0.0001, 0.0},       //  p       1           0       // a small to speed up the "sampling", the value itself does not mean anything
-            {2.608, 0.513,  -0.51},     //  O       16          1
-            {3.34,  0.58,   -0.233},    //  Si      28          2
-            {2.54,  2.191,  0.16},      //  S       32          3
-            {3.766, 0.586,  -0.161},    //  Ca      40          4
-            {4.309, 0.517,  -0.1308},   //  Ni      58          5
-            {4.2,   0.596,  0.0},       //  Cu      62,63       6
-            {5.36,  0.59,   0.0},       //  Xe      129         7
-            {6.58,  0.48,   0.0},       //  W       186         8
-            {6.38,  0.535,  0.0},       //  Au      197         9
-            {6.62,  0.546,  0.0},       //  Pb      207,208     10
-        };
+    // from source [1] and [2]
+    const NucleusParameters params[] =
+    { // R,     a,      w               Symbol  A           index
+        {1.0,   0.0001, 0.0},       //  p       1           0       // surface diffusiveness small to speed up the "sampling", the value itself does not mean anything
+        {2.608, 0.513,  -0.51},     //  O       16          1
+        {3.34,  0.58,   -0.233},    //  Si      28          2
+        {2.54,  2.191,  0.16},      //  S       32          3
+        {3.766, 0.586,  -0.161},    //  Ca      40          4
+        {4.309, 0.517,  -0.1308},   //  Ni      58          5
+        {4.2,   0.596,  0.0},       //  Cu      62,63       6
+        {5.36,  0.59,   0.0},       //  Xe      129         7
+        {6.58,  0.48,   0.0},       //  W       186         8
+        {6.38,  0.535,  0.0},       //  Au      197         9
+        {6.62,  0.546,  0.0},       //  Pb      207,208     10
+    };
 
+    inline const NucleusParameters& get (uint atomic_num)
+    {
         switch (atomic_num)
         {
             case 1:
