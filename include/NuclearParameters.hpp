27ac1e9e10e8c42295f3fec966cac76aec4c0784
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2025-07-08 15:11:08 +0200

    chore(parameters):
    -introduce enum and struct for clarity

--- include/NuclearParameters.hpp
+++ include/NuclearParameters.hpp
@@ -31,59 +31,78 @@ struct NucleusParameters {
   double deviation_from_spherical;    // w
 };
 
-namespace NuclearParameters {
-// from source [1] and [2]
-const NucleusParameters params[] = {
-    // R, a,      w               Symbol  A           index
-    {1.0, 0.0001, 0.0},       //  p       1           0
-    {2.608, 0.513, -0.51},    //  O       16          1
-    {3.34, 0.58, -0.233},     //  Si      28          2
-    {2.54, 2.191, 0.16},      //  S       32          3
-    {3.766, 0.586, -0.161},   //  Ca      40          4
-    {4.309, 0.517, -0.1308},  //  Ni      58          5
-    {4.2, 0.596, 0.0},        //  Cu      62,63       6
-    {5.36, 0.59, 0.0},        //  Xe      129         7
-    {6.58, 0.48, 0.0},        //  W       186         8
-    {6.38, 0.535, 0.0},       //  Au      197         9
-    {6.62, 0.546, 0.0},       //  Pb      207,208     10
+enum Type : uint {
+  p = 1,
+  O16 = 16,
+  Si28 = 28,
+  S32 = 32,
+  Ca40 = 40,
+  Ni58 = 58,
+  Cu62 = 62,
+  Cu63 = 63,
+  Xe129 = 129,
+  W186 = 186,
+  Au197 = 197,
+  Pb207 = 207,
+  Pb208 = 208,
 };
+
+const struct {
+  NucleusParameters p = {1.0, 0.0001, 0.0};
+  NucleusParameters O16 = {2.608, 0.513, -0.51};
+  NucleusParameters Si28 = {3.34, 0.58, -0.233};
+  NucleusParameters S32 = {2.54, 2.191, 0.16};
+  NucleusParameters Ca40 = {3.766, 0.586, -0.161};
+  NucleusParameters Ni58 = {4.309, 0.517, -0.1308};
+  NucleusParameters Cu62 = {4.2, 0.596, 0.0};
+  NucleusParameters Cu63 = Cu62;
+  NucleusParameters Xe129 = {5.36, 0.59, 0.0};
+  NucleusParameters W186 = {6.58, 0.48, 0.0};
+  NucleusParameters Au197 = {6.38, 0.535, 0.0};
+  NucleusParameters Pb207 = {6.62, 0.546, 0.0};
+  NucleusParameters Pb208 = Pb207;
+} params;
 
 inline const NucleusParameters& get(uint atomic_num) {
   switch (atomic_num) {
-    case 1:
-      return params[0];
+    case p:
+      return params.p;
 
-    case 16:
-      return params[1];
+    case O16:
+      return params.O16;
 
-    case 28:
-      return params[2];
+    case Si28:
+      return params.Si28;
 
-    case 32:
-      return params[3];
+    case S32:
+      return params.S32;
 
-    case 40:
-      return params[4];
+    case Ca40:
+      return params.Ca40;
 
-    case 58:
-      return params[5];
+    case Ni58:
+      return params.Ni58;
 
-    case 62:
-    case 63:
-      return params[6];
+    case Cu62:
+      return params.Cu62;
 
-    case 129:
-      return params[7];
+    case Cu63:
+      return params.Cu63;
 
-    case 186:
-      return params[8];
+    case Xe129:
+      return params.Xe129;
 
-    case 197:
-      return params[9];
+    case W186:
+      return params.W186;
 
-    case 207:
-    case 208:
-      return params[10];
+    case Au197:
+      return params.Au197;
+
+    case Pb207:
+      return params.Pb207;
+
+    case Pb208:
+      return params.Pb208;
 
     default:
       std::cerr << ERROR_NO_DATA << std::endl;
